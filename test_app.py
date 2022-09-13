6a89ea4db94007749e6939ae52005f645812f320
debatosh99 <63183667+debatosh99@users.noreply.github.com>
2022-09-13 23:44:14 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,6 +1,7 @@
 import unittest
 import requests
-#import com.anz.cloud.app1
+import dbInitializer
+import app
 
 class TestApp(unittest.TestCase):
 
@@ -8,15 +9,24 @@ class TestApp(unittest.TestCase):
     baseurl = 'http://192.168.0.110:8080/v1/books'
     testbook1 = {"name":"Atomic Habits", "author": "Mr India", "published_date": "22/09/07", "price": 700, "in_stock": "Yes"}
 
+    def setUp(self) -> None:
+        dbInitializer.createDBandTable("Library2.db")
+        dbInitializer.insertSampleRecords()
+        dbInitializer.printAllRecords()
+        dbInitializer.filterAndGetJson()
 
     def test_get_book(self):
-        res = requests.get(TestApp.baseurl)
-        self.assertEqual(res.status_code, 200)
+        res1 = requests.get(TestApp.baseurl)
+        self.assertEqual(res1.status_code, 200)
+        res2 = requests.get(TestApp.baseurl+'/')
+        self.assertEqual(res2.status_code, 404)
 
     def test_create_book(self):
         res = requests.post(TestApp.baseurl, json=TestApp.testbook1)
         self.assertEqual(res.status_code, 200)
         self.assertIsInstance(res.json()['id'], int)
+        res = requests.post(TestApp.baseurl, json=TestApp.testbook1)
+        self.assertEqual(res.status_code, 403)
 
     def test_get_books(self):
         res = requests.get(TestApp.baseurl+'?name="Math*"')
@@ -31,10 +41,20 @@ class TestApp(unittest.TestCase):
         self.assertEqual(res2.status_code, 200)
         self.assertEqual(len(res2.json()), 0)
 
+    def test_dbutil(self):
+        customsql = "SELECT count(*) FROM bookstore WHERE name = '{}'".format(str("vague"))
+        fetch = 'one'
+        result = app.dbutils(customsql, fetch)
+        self.assertEqual(result, 0)
+
+
 
 
 
 if __name__ == '__main__':
+#    dosetup()
     unittest.main()
+
+
 
 
