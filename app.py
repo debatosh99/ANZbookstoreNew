95b3af2a9c2ad24a5c1ea5ae4f8603751735fcc6
debatosh99 <63183667+debatosh99@users.noreply.github.com>
2022-09-13 23:43:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,17 +9,19 @@ import config as config
 
 app = Flask(__name__)
 
-
-@app.route('/v1/books/<int:book_id>', methods=['GET'])
-def get_book(book_id):
+def dbutils(customsql, fetch):
     try:
         con = sql.connect("Library2.db")
         con.row_factory = sql.Row
         cur = con.cursor()
-        cur.execute("SELECT * FROM bookstore WHERE id = ?", [int(book_id)])
-        #data = cur.fetchone()
-        result = [dict(row) for row in cur.fetchall()]
-        #print(result)
+        print(".............................")
+        print(customsql)
+        cur.execute(customsql)
+        con.commit()
+        if fetch == 'all':
+            result = [dict(row) for row in cur.fetchall()]
+        else:
+            result = cur.fetchone()[0]
     except sql.Error as error:
         print("SQL Error", error)
         return jsonify({"message": "Retry after sometime"}), HTTPStatus.SERVICE_UNAVAILABLE
@@ -28,24 +30,30 @@ def get_book(book_id):
         return jsonify({"message": "Retry after sometime"}),HTTPStatus.SERVICE_UNAVAILABLE
     else:
         print("DB connection success")
+        print(result)
+        return result
     finally:
         con.close()
 
+
+
+@app.route('/v1/books/<int:book_id>', methods=['GET'])
+def get_book(book_id):
+    customsql = "SELECT * FROM bookstore WHERE id = {}".format(int(book_id))
+    fetch = 'all'
+    result = dbutils(customsql, fetch)
     if result is None:
         return jsonify({"message": "Book not found"}),HTTPStatus.NOT_FOUND
     else:
         json_output = json.dumps(result)
         return jsonify(result), 200
-        #return jsonify(result), 200
+
 
 
 @app.route('/v1/books', methods=['POST'])
 def create_book():
     data = request.json
-    #data = request.form.to_dict()
     print(data)
-    #data = json.loads(request.form.get('data'))
-    #data = request.get_json()
     name = data.get('name')
     author = data.get('author')
     published_date_user = data.get('published_date')
@@ -53,74 +61,43 @@ def create_book():
     price = data.get('price')
     in_stock = data.get('in_stock')
 
-    try:
-        con = sql.connect("Library2.db")
-        cur = con.cursor()
-        cur.execute("insert into bookstore(name,author,published_date,price,in_stock) values (?,?,?,?,?)", (name, author, published_date, price, in_stock))
-        con.commit()
-        cur.execute("SELECT id FROM bookstore WHERE name = ?", [str(name)])
-        id = cur.fetchone()[0]
-        print(id)
-    except sql.Error as error:
-        print("SQL Error", error)
-        return jsonify({"message": "Retry after sometime"}), HTTPStatus.SERVICE_UNAVAILABLE
-    except Exception as exp:
-        print("DB connection failed")
-        return jsonify({"message": "Retry after sometime"}),HTTPStatus.SERVICE_UNAVAILABLE
-    else:
-        print("DB connection success")
-    finally:
-        con.close()
+    # Check if the record with same book name is already present in the DB
+    customsql = "SELECT count(*) FROM bookstore WHERE name = '{}'".format(str(name))
+    fetch = 'one'
+    result = dbutils(customsql, fetch)
+    if result != 0:
+        return jsonify({"message": "Book already present"}), HTTPStatus.FORBIDDEN
 
-    return jsonify({"id":id}), 200
+    # If the record is a new record not present in DB then insert into DB
+    customsql = "insert into bookstore(name,author,published_date,price,in_stock) values ('{}','{}','{}',{},'{}')".format(name, author, published_date, price, in_stock)
+    fetch = 'one'
+    result = dbutils(customsql, fetch)
+
+    # Get the id of the newly inserted record
+    customsql = "SELECT id FROM bookstore WHERE name = '{}'".format(str(name))
+    fetch = 'one'
+    id = dbutils(customsql, fetch)
+    print(id)
+    return jsonify({"id": id}), 200
+
+
 
 
 @app.route('/v1/books', methods=['GET'])
 def get_books():
     print(len(request.args))
     if len(request.args) == 0:
-        try:
-            con = sql.connect("Library2.db")
-            con.row_factory = sql.Row
-            cur = con.cursor()
-            cur.execute("SELECT * FROM bookstore")
-            result = [dict(row) for row in cur.fetchall()]
-            #data = cur.fetchall()
-        except sql.Error as error:
-            print("SQL Error", error)
-            return jsonify({"message": "Retry after sometime"}), HTTPStatus.SERVICE_UNAVAILABLE
-        except Exception as exp:
-            print("DB connection failed")
-            return jsonify({"message": "Retry after sometime"}), HTTPStatus.SERVICE_UNAVAILABLE
-        else:
-            print("DB connection success")
-        finally:
-            con.close()
-
+        customsql = "SELECT * FROM bookstore"
+        fetch = 'all'
+        result = dbutils(customsql, fetch)
         json_output = json.dumps(result)
         return json_output, 200
     else:
         name = request.args.get('name')
         name = name.replace('*', '%')
-        try:
-            con = sql.connect("Library2.db")
-            con.row_factory = sql.Row
-            cur = con.cursor()
-            query = "SELECT * FROM bookstore WHERE name like {}".format(name)
-            cur.execute(query)
-            result = [dict(row) for row in cur.fetchall()]
-            #data = cur.fetchall()
-        except sql.Error as error:
-            print("SQL Error", error)
-            return jsonify({"message": "Retry after sometime"}), HTTPStatus.SERVICE_UNAVAILABLE
-        except Exception as exp:
-            print("DB connection failed")
-            return jsonify({"message": "Retry after sometime"}), HTTPStatus.SERVICE_UNAVAILABLE
-        else:
-            print("DB connection success")
-        finally:
-            con.close()
-
+        customsql = "SELECT * FROM bookstore WHERE name like {}".format(name)
+        fetch = 'all'
+        result = dbutils(customsql, fetch)
         if result is None:
             return jsonify({"message": "Book not found"}), HTTPStatus.NOT_FOUND
         else:
